7a18b1a8ef857fbeec632bc289503389403414ac
abhayminer01 <abhayvapvl@gmail.com>
2025-08-28 08:13:06 +0530

    server init

---
+++ server/middlewares/auth.js
@@ -0,0 +1,9 @@
+const authMiddleware = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = authMiddleware;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth.routes');
+const vehicleRoutes = require('./routes/vehicle.routes');
+const userRoutes = require('./routes/user.routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`);
+});
